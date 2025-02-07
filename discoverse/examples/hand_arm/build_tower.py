d514eb4c210ad9615f7a27e6b14e45c5277a8db5
刘天行 <2499323877@qq.com>
2025-02-07 15:53:33 +0800

    编写搭积木场景的.xml文件，开始编写抓取部分

---
+++ discoverse/examples/hand_arm/build_tower.py
@@ -0,0 +1,19 @@
+import mujoco
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+import os
+import shutil
+import argparse
+import multiprocessing as mp
+
+import traceback
+from discoverse.airbot_play import AirbotPlayIK_nopin #使用这个进行逆运动学解算无需URDF
+from discoverse import DISCOVERSE_ROOT_DIR , DISCOVERSE_ASSERT_DIR #引入仿真器路径和模型路径
+
+from discoverse.utils import get_body_tmat , step_func , SimpleStateMachine #获取旋转矩阵，步进，状态机
+
+
+class SimNode():
+    def __init__(self):
+        pass
